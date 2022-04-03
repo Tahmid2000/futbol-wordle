cc49b5c54059e7d84e3bad3a400b8990eccc6465
Tahmid Imran <tahmidimran1@gmail.com>
2022-04-02 19:38:28 -0500

    Add FastAPI

--- data.py
+++ data.py
@@ -1,5 +1,21 @@
 import pandas as pd
-df = pd.read_csv("players_22.csv", low_memory=False)
-dfclean = df[['short_name', 'player_positions', 'club_name', 'league_name',
-              'nationality_name', 'player_face_url', 'club_logo_url', 'nation_flag_url']].head(1000)
-print(dfclean)
+from random import randrange
+
+
+class Player:
+
+    def __init__(self):
+        self.allPlayers()
+
+    def allPlayers(self):
+        df = pd.read_csv("players_22.csv", low_memory=False)
+        self.possiblePlayers = df[['short_name', 'player_positions', 'club_name', 'league_name',
+                                   'nationality_name', 'player_face_url', 'club_logo_url', 'nation_flag_url']].head(2500)
+        self.possibleAnswers = self.possiblePlayers.head(500)
+
+    def randomPlayer(self):
+        num = randrange(500)
+        return self.possibleAnswers.iloc[[num]]
+
+    def playersFromQuery(self, query):
+        return self.possiblePlayers[self.possiblePlayers['short_name'].str.contains(query, case=False)]
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from data import Player
+
+app = FastAPI()
+player = Player()
+
+
+@app.get("/")
+async def root():
+    return {"player": player.randomPlayer()['short_name']}
+
+
+@app.get("/player/{query}")
+async def getPlayerFromQuery(query: str):
+    tempdf = player.playersFromQuery(query)
+    result = []
+    for index, row in tempdf.iterrows():
+        result.append({"short_name": row['short_name'],
+                      "player_positions": row['player_positions'], "club_name": row['club_name'], 'league_name': row['league_name'], 'nationality_name': row['nationality_name'], 'player_face_url': row['player_face_url'], 'club_logo_url': row['club_logo_url'], 'nation_flag_url': row['nation_flag_url']})
+    return {"results": result}
