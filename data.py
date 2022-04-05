9577a331e320f3890401091b72cdc273b39c0133
raiyanmahbub <raiyan.mahbub00@gmail.com>
2022-04-05 01:03:21 -0500

    position check code, still need to test for edge cases

--- data.py
+++ data.py
@@ -31,9 +31,13 @@ class Player:
             'AF': 'Africa',
             'EU': 'Europe'
         }
-        country_code = pc.country_name_to_country_alpha2(country, cn_name_format="default")
-        continent_name = pc.country_alpha2_to_continent_code(country_code)
-        return continents[continent_name]
+
+        try:
+            country_code = pc.country_name_to_country_alpha2(country, cn_name_format="default")
+            continent_name = pc.country_alpha2_to_continent_code(country_code)
+            return continents[continent_name]
+        except:
+            return 'Europe'
 
 
     def playersFromQuery(self, query):
@@ -54,8 +58,32 @@ class Player:
             p2.append({"short_name": row['short_name'], "player_positions": row['player_positions'], "club_name": row['club_name'], 'league_name': row['league_name'], 'nationality_name': row['nationality_name'], 'player_face_url': row['player_face_url'], 'club_logo_url': row['club_logo_url'], 'nation_flag_url': row['nation_flag_url']})
 
         # Position Check
+        positionMap = {'attack': ['LW', 'RW', 'ST', 'CF'],
+                        'midfield': ['CM', 'CAM', 'CDM'],
+                        'defense': ['CB', 'LB', 'RB', 'GK']}
+        p1_position = p1[0]['player_positions'].split(',')[0]
+        p2_position = p2[0]['player_positions'].split(',')[0]
 
+        if p1_position in positionMap['attack']:
+            p1_role = 'attack'
+        elif p1_position in positionMap['defense']:
+            p1_role = 'defense'
+        else:
+            p1_role = 'midfield'
 
+        if p2_position in positionMap['attack']:
+            p2_role = 'attack'
+        elif p2_position in positionMap['defense']:
+            p2_role = 'defense'
+        else:
+            p2_role = 'midfield'
+        
+        if (p1_position == p2_position):
+            final['position'] = 'green'
+        elif (p1_role == p2_role):
+            final['position'] = 'yellow'
+        else:
+            final['position'] = 'black'
 
         # Country Check
         if (p1[0]['nationality_name'] == p2[0]['nationality_name']):
@@ -64,7 +92,6 @@ class Player:
             final['country'] = 'yellow'
         else:
             final['country'] = 'black'
-
 
         # Club Check
         if (p1[0]['club_name'] == p2[0]['club_name']):
@@ -81,4 +108,4 @@ class Player:
 
 
 player = Player()
-player.matchingPlayer('neymar', 'messi')    
+player.matchingPlayer('benzema', 'messi')    
